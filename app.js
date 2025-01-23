38590264adfc1cd44a194093b2cf188111045b64
cmrondon2000 <cm.rondon2000@gmail.com>
2025-01-23 10:01:23 -0300

    Se colocaron las funciones

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+
+function agregarAmigo() {
+    let amigoIngresado = document.getElementById("amigo").value;
+    if (amigoIngresado == null){
+        alert("Por favor, inserte un nombre.")
+    } else {
+        amigos.push(amigoIngresado);
+    }
+
+}
+
+
+function sortearAmigo() {
+    
+}
